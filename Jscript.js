b5b1df9934ab13a8aae4ce0d54865d35f4239e49
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-04-21 15:43:39 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -1,12 +1,3 @@
-// The function gets called when the window is fully loaded
-window.onload = function() {
-
-    // Define the image dimensions
-    var width = canvas.width;
-    var height = canvas.height;
-
-}
-
 var Bpixels = new bpixels({
     id: 'particles',
     width: window.innerWidth,
@@ -19,16 +10,6 @@ var Bpixels = new bpixels({
     backgroundTo: [25, 50, 150],
     backgroundDuration: 4000,
     nobg: false,
-    number: window.hasOwnProperty('orientation') ? 30 : 100,
     speed: 20,
     pointerCircleRadius: 150
 });
-
-function makebackground(offset) {
-    for (var x = 0; x < width; x++) {
-        for (var y = 0; y < width; y++) {
-            var pixelindex = (y * width + x);
-
-        }
-    }
-}
