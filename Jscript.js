98c05978c515e0f958b44a345fe1b871345b2249
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-04-21 15:51:19 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -9,27 +9,6 @@
  * 
  */
 
-
-
-var nodesjs = new NodesJs({
-    id: 'nodes',
-    width: window.innerWidth,
-    height: window.innerHeight,
-    particleSize: 2,
-    lineSize: 1,
-    particleColor: [255, 255, 255, 0.3],
-    lineColor: [255, 255, 255],
-    backgroundFrom: [10, 25, 100],
-    backgroundTo: [25, 50, 150],
-    backgroundDuration: 4000,
-    nobg: false,
-    number: window.hasOwnProperty('orientation') ? 30 : 100,
-    speed: 20,
-    pointerCircleRadius: 150
-});
-
-
-
 var NodesJs = (function(parameters) {
     t_NodesJs = this;
     t_NodesJs.id = parameters.id;
