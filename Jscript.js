c2081197b172fd33eff6ba25ab74759f7ad5bf61
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-04-21 16:27:51 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -1,14 +1,4 @@
-/*
- * nodes.js is a nodes/particles animation useable for backgrounds
- *
- * http://oguzhaneroglu.com/projects/nodes.js/
- * https://github.com/rohanrhu/nodes.js
- *
- * Copyright (C) 2018, Oğuzhan Eroğlu <rohanrhu2@gmail.com>
- * Licensed under MIT
- * 
- */
-
+/* Defining the Nodes */
 var NodesJs = (function(parameters) {
     t_NodesJs = this;
     t_NodesJs.id = parameters.id;
