2d3129b34893226bee30cf562e823d8620c65f3f
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-04-21 15:33:08 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -1,3 +1,20 @@
+// The function gets called when the window is fully loaded
+window.onload = function() {
+
+    // Define the image dimensions
+    var width = canvas.width;
+    var height = canvas.height;
+
+}
 var Bpixels = {
-    pixels = 100
+
+}
+
+function makebackground(offset) {
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < width; y++) {
+            var pixelindex = (y * width + x);
+
+        }
+    }
 }
