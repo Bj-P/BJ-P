f7f6c8633f296dc3520f8eeed80ee043ebba7d26
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-04-21 15:49:17 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -6,7 +6,29 @@
  *
  * Copyright (C) 2018, Oğuzhan Eroğlu <rohanrhu2@gmail.com>
  * Licensed under MIT
+ * 
  */
+
+
+
+var nodesjs = new NodesJs({
+    id: 'nodes',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    particleSize: 2,
+    lineSize: 1,
+    particleColor: [255, 255, 255, 0.3],
+    lineColor: [255, 255, 255],
+    backgroundFrom: [10, 25, 100],
+    backgroundTo: [25, 50, 150],
+    backgroundDuration: 4000,
+    nobg: false,
+    number: window.hasOwnProperty('orientation') ? 30 : 100,
+    speed: 20,
+    pointerCircleRadius: 150
+});
+
+
 
 var NodesJs = (function(parameters) {
     t_NodesJs = this;
