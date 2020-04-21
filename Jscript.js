8f2c442fca66f5782b42fc47ee84aa04e059943f
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-04-21 15:42:09 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -6,9 +6,23 @@ window.onload = function() {
     var height = canvas.height;
 
 }
-var Bpixels = {
 
-}
+var Bpixels = new bpixels({
+    id: 'particles',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    particleSize: 2,
+    lineSize: 1,
+    particleColor: [255, 255, 255, 0.3],
+    lineColor: [255, 255, 255],
+    backgroundFrom: [10, 25, 100],
+    backgroundTo: [25, 50, 150],
+    backgroundDuration: 4000,
+    nobg: false,
+    number: window.hasOwnProperty('orientation') ? 30 : 100,
+    speed: 20,
+    pointerCircleRadius: 150
+});
 
 function makebackground(offset) {
     for (var x = 0; x < width; x++) {
