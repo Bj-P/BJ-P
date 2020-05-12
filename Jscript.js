41d264dc25c28d91784909fb7e7d01bc9356ec3d
Bj-P <59878041+Bj-P@users.noreply.github.com>
2020-05-12 15:47:42 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -19,13 +19,13 @@ window.onload = function() {
                 // Get the pixel index
                 var pixelindex = (y * width + x) * 4;
 
-                // Generate a xor pattern with some random noise
-                var red = ((x + offset) % 256) ^ ((y + offset) % 256);
+                // Generate a xor pattern with some random noise -> size of the Pixels
+                var red = ((x + offset + 2) % 256) ^ ((y + offset + 2) % 256);
                 var green = ((2 * x + offset) % 256) ^ ((2 * y + offset) % 256);
                 var blue = 50 + Math.floor(Math.random() * 100);
 
                 // Rotate the colors
-                blue = (blue + offset) % 256;
+                blue = (blue + offset * 9) % 256;
 
                 // Set the pixel data
                 imagedata.data[pixelindex] = red; // Red
